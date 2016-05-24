c3694d054691d4a9c5a2724894782e51bca45691
Raomuyang <raomengnan@qq.com>
2016-05-24 19:02:47 +0800

    修改线程池中的示例函数

--- tools/ThreadPool.py
+++ tools/ThreadPool.py
@@ -74,10 +74,9 @@ if __name__ == '__main__':
         print(i)
         time.sleep(10)
 
-    pool = ThreadPool(10,10)
+    pool = ThreadPool(10)
     arg = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     for i in arg:
         args = [i]
         pool.addTask(test,args)
-        # pool.checkThread.join()
 
